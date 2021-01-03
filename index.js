9230592656e9f5665f344a16faff5cb259c6d58b
Jintarq <71939344+Jintarq@users.noreply.github.com>
2021-01-03 20:59:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const githubRepo = document.querySelector(".githubrepo");
+const repoName = document.querySelector(".repo-name");
+const repoArr = [];
+
+function requestUserRepos(username) {
+  // Create new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+
+  // GitHub endpoint, dynamically passing in specified username
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  // Open a new connection, using a GET request via URL endpoint
+  // Providing 3 arguments (GET/POST, The URL, Async True/False)
+  xhr.open("GET", url, true);
+
+  // When request is received
+  // Process it here
+  xhr.onload = function () {
+    // Parse API data into JSON
+    const data = JSON.parse(this.response);
+
+    // Log the response
+    console.log(data);
+
+    // Loop over each object in data array
+    for (let i in data) {
+      // Log the repo name
+      console.log("Repo:", data[i].name);
+      repoArr.push(data[i].name);
+      console.log(repoArr);
+    }
+    // For each element, create a div and stock him in a "p" element
+    repoArr.forEach((element) => {
+      const newrepoDiv = document.createElement("div");
+      githubRepo.appendChild(newrepoDiv);
+      newrepoDiv.classList.add("newrepoDiv");
+      const newRepo = document.createElement("p");
+      newrepoDiv.appendChild(newRepo);
+      newRepo.innerHTML = element;
+      newRepo.classList.add("newRepo");
+    });
+  };
+
+  // Send the request to the server
+  xhr.send();
+}
+// Choose the username you want
+requestUserRepos("Jintarq");
